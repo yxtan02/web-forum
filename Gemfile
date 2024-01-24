07600a19e28794021b6b23b6641373d539fd8980
yxtan02 <85004454+yxtan02@users.noreply.github.com>
2024-01-24 21:10:17 +0800

    Migrate data to PostgreSQL

--- Gemfile
+++ Gemfile
@@ -4,7 +4,8 @@ ruby "3.2.2"
 
 gem "rails", "~> 7.1.2"
 gem "sprockets-rails"
-gem "sqlite3", "~> 1.4"
+# gem "sqlite3", "~> 1.4"
+gem "pg"
 gem "puma", ">= 5.0"
 gem "jsbundling-rails"
 gem "turbo-rails"
@@ -15,6 +16,7 @@ gem 'bcrypt', '~> 3.1', '>= 3.1.20'
 gem 'rack-cors', :require => 'rack/cors'
 gem "tzinfo-data", platforms: %i[ windows jruby ]
 gem "bootsnap", require: false
+gem 'seed_dump'
 
 group :development, :test do
   gem "debug", platforms: %i[ mri windows ]
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,13 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_01_18_072535) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.text "content"
-    t.integer "post_id", null: false
-    t.integer "user_id", null: false
+    t.bigint "post_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
