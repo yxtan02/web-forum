ca9c041d4ddf0ec7484d26c21d651e814aafbab8
yxtan02 <yxtan02@gmail.com>
2025-01-24 00:47:50 +0800

    Update deployment script

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,7 +3,5 @@
 set -o errexit
 
 bundle install
-./bin/rails assets:precompile
-./bin/rails assets:clean
-./bin/rails db:migrate
-./bin/rails db:seed
+bundle exec rails assets:precompile
+bundle exec rails assets:clean
